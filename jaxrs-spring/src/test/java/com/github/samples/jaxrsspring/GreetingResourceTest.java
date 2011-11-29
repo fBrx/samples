07fa542ab229abc13019a99c53255b7d31d2fc44
Florian Mueller <admin@florian-m.net>
2011-11-29 17:41:30 +0100

    - removed unused log4j config - added test case

---
+++ jaxrs-spring/src/test/java/com/github/samples/jaxrsspring/GreetingResourceTest.java
@@ -0,0 +1,28 @@
+package com.github.samples.jaxrsspring;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class GreetingResourceTest {
+
+	GreetingResource gr = new GreetingResource();
+	
+	@Test
+	public void test_greet(){
+		Assert.assertEquals("howdy, stranger!", gr.greet());
+	}
+	
+	@Test
+	public void test_greetMe_null() {
+		Assert.assertEquals(gr.greet(), gr.greetMe(null));
+	}
+	
+	@Test
+	public void test_greetMe_flo() {
+		Assert.assertEquals("whuuuuuuuzzzuuuuupp, flo", gr.greetMe("flo"));
+	}
+}
