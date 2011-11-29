fd8e57a39f9adc78091a161008d9f9e14483592a
Florian Mueller <admin@florian-m.net>
2011-11-29 17:53:04 +0100

    added dateservice implementation

---
+++ jaxrs-spring/src/main/java/com/github/samples/jaxrsspring/DateService.java
@@ -0,0 +1,21 @@
+package com.github.samples.jaxrsspring;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Simple class which formats the current date and time in a given format.
+ *
+ * @author Florian Müller
+ * @see SimpleDateFormat
+ */
+@Component
+public class DateService {
+
+	public String getDate(String pattern) {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		return sdf.format(new Date());
+	}
+}
