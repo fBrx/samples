884c158240d20f4270e167f1193d95f31cb1b05a
schali <admin@florian-m.net>
2012-07-14 00:54:46 +0200

    added evaluation of WebServiceContext

--- jaxws-provider/src/main/java/com/github/schali/samples/jaxwsprovider/GreetingService.java
+++ jaxws-provider/src/main/java/com/github/schali/samples/jaxwsprovider/GreetingService.java
@@ -1,16 +1,23 @@
 package com.github.schali.samples.jaxwsprovider;
 
+import javax.annotation.Resource;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
 
 @WebService
 public class GreetingService {
 
+	@Resource
+	WebServiceContext ctx;
+	
 	@WebResult(name="greeting")
 	public String greetMe(@WebParam(name="name") String name) {
-		
-		return "Howdy, " + name + "!";
+		if(ctx == null || ctx.getUserPrincipal() == null)
+			return "Howdy, " + name + "!";
+		else
+			return "Howdy, " + name + "! You are authenticated as " + ctx.getUserPrincipal().getName() + ".";
 	}
 	
 }
