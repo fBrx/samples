3b717c47991b8d75a5f522d826822a5eb0c318ce
schali <admin@florian-m.net>
2012-08-03 18:37:52 +0200

    formatted source

--- jvm-maintenance/src/main/java/com/github/samples/jvmmaintenance/ActionServlet.java
+++ jvm-maintenance/src/main/java/com/github/samples/jvmmaintenance/ActionServlet.java
@@ -1,5 +1,6 @@
 package com.github.samples.jvmmaintenance;
 
+
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
@@ -10,100 +11,99 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ActionServlet extends HttpServlet{
 
-	private static final long serialVersionUID = 1L;
-	
-	/** The {@link Logger} of the class */
-	private static final Logger LOG = Logger.getLogger(ActionServlet.class.getName());
+public class ActionServlet extends HttpServlet {
 
-	/** name of the parameter which specifies the operation to execute */
-	public static final String OPERATION_PARAM = "operation";
-	
-	/** constants for all available operations. the values are used as arguments for the {@link ActionServlet#OPERATION_PARAM} parameter */
-	public static final String ACTION_GC = "GC";
-	public static final String ACTION_JAVADUMPIBM = "JAVADUMPIBM";
-	public static final String ACTION_HEAPDUMPIBM = "HEAPDUMPIBM";
-	public static final String ACTION_SYSTEMDUMPIBM = "SYSTEMDUMPIBM";
-	
+    private static final long   serialVersionUID     = 1L;
 
-	/**
-	 * Simply calls {@link #doGet(HttpServletRequest, HttpServletResponse)}
-	 * 
-	 * @see #doGet(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
-	}
-	
-	/**
-	 * Evaluates the {@link ActionServlet#OPERATION_PARAM} request parameter and performs the corresponding operations.
-	 * The response is then redirected to "/" which causes the default welcome page to be opened.
-	 * 
-	 * @see #generateHeapDumpIbm(HttpServletRequest, HttpServletResponse)
-	 * @see #performGarbageCollection(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String operation = req.getParameter(OPERATION_PARAM);
-		LOG.info("operation: " + operation);
-		
-		if(operation != null && operation.equals(ACTION_GC))
-			performGarbageCollection();
-		
-		else if(operation != null &&
-				(operation.equals(ACTION_JAVADUMPIBM) ||
-				 operation.equals(ACTION_HEAPDUMPIBM) ||
-				 operation.equals(ACTION_SYSTEMDUMPIBM)))
-			generateIbmDump(operation);
+    /** The {@link Logger} of the class */
+    private static final Logger LOG                  = Logger.getLogger(ActionServlet.class.getName());
 
-		resp.sendRedirect("");
-			
-	}
-	
-	/**
-	 * Generates a HeapDump on a IBM JVM.
-	 *  
-	 * @param req the {@link HttpServletRequest}
-	 * @param resp the {@link HttpServletResponse}
-	 * @param operation 
-	 */
-	private void generateIbmDump(String operation) {
-		LOG.info("generating ibm dump: " + operation);
-		try{
+    /** name of the parameter which specifies the operation to execute */
+    public static final String  OPERATION_PARAM      = "operation";
+
+    /** constants for all available operations. the values are used as arguments for the {@link ActionServlet#OPERATION_PARAM} parameter */
+    public static final String  ACTION_GC            = "GC";
+    public static final String  ACTION_JAVADUMPIBM   = "JAVADUMPIBM";
+    public static final String  ACTION_HEAPDUMPIBM   = "HEAPDUMPIBM";
+    public static final String  ACTION_SYSTEMDUMPIBM = "SYSTEMDUMPIBM";
+
+    /**
+     * Simply calls {@link #doGet(HttpServletRequest, HttpServletResponse)}
+     * 
+     * @see #doGet(HttpServletRequest, HttpServletResponse)
+     */
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    /**
+     * Evaluates the {@link ActionServlet#OPERATION_PARAM} request parameter and performs the corresponding operations. The response is then redirected to "/" which causes the default welcome page to
+     * be opened.
+     * 
+     * @see #generateHeapDumpIbm(HttpServletRequest, HttpServletResponse)
+     * @see #performGarbageCollection(HttpServletRequest, HttpServletResponse)
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String operation = req.getParameter(OPERATION_PARAM);
+        LOG.info("operation: " + operation);
+
+        if (operation != null && operation.equals(ACTION_GC))
+            performGarbageCollection();
+
+        else if (operation != null && (operation.equals(ACTION_JAVADUMPIBM) || operation.equals(ACTION_HEAPDUMPIBM) || operation.equals(ACTION_SYSTEMDUMPIBM)))
+
+        generateIbmDump(operation);
+
+        resp.sendRedirect("");
+
+    }
+
+    /**
+     * Generates a HeapDump on a IBM JVM.
+     * 
+     * @param req
+     *            the {@link HttpServletRequest}
+     * @param resp
+     *            the {@link HttpServletResponse}
+     * @param operation
+     */
+    private void generateIbmDump(String operation) {
+        LOG.info("generating ibm dump: " + operation);
+        try {
             // get object for ibm Dump class
             Class ibmHeap = Class.forName("com.ibm.jvm.Dump");
-			
-			//get handler to methods
-			Method javaDump = ibmHeap.getDeclaredMethod("JavaDump", new Class[] {});
-			Method heapDump = ibmHeap.getDeclaredMethod("HeapDump", new Class[] {});
-			Method systemDump = ibmHeap.getDeclaredMethod("SystemDump", new Class[] {});
-			
-			//invoke method according to operation
-			if(operation.equals(ACTION_JAVADUMPIBM))
-				javaDump.invoke(null, (Object[])null);
-			if(operation.equals(ACTION_HEAPDUMPIBM))
-				heapDump.invoke(null, (Object[])null);
-			if(operation.equals(ACTION_SYSTEMDUMPIBM))
-				systemDump.invoke(null, (Object[])null);
-			
-		}catch (Exception ex) {
-			LOG.log(Level.SEVERE, "error while generating ibm dump (" + operation + "): " + ex.getMessage(), ex);
-		}
-	}
 
-	/**
-	 * Request a garbage collection cycle.
-	 *  
-	 * @param req the {@link HttpServletRequest}
-	 * @param resp the {@link HttpServletResponse}
-	 * @see System#gc()
-	 */
-	private void performGarbageCollection() {
-		LOG.info("perfoming gc");
-		try{
-			System.gc();
-		}catch (Exception ex) {
-			LOG.log(Level.SEVERE, "error while performing gc: " + ex.getMessage(), ex);
-		}
-	}
+            // get handler to methods
+            Method javaDump = ibmHeap.getDeclaredMethod("JavaDump", new Class[] {});
+            Method heapDump = ibmHeap.getDeclaredMethod("HeapDump", new Class[] {});
+            Method systemDump = ibmHeap.getDeclaredMethod("SystemDump", new Class[] {});
+
+            // invoke method according to operation
+            if (operation.equals(ACTION_JAVADUMPIBM)) javaDump.invoke(null, (Object[]) null);
+            if (operation.equals(ACTION_HEAPDUMPIBM)) heapDump.invoke(null, (Object[]) null);
+            if (operation.equals(ACTION_SYSTEMDUMPIBM)) systemDump.invoke(null, (Object[]) null);
+
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "error while generating ibm dump (" + operation + "): " + ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Request a garbage collection cycle.
+     * 
+     * @param req
+     *            the {@link HttpServletRequest}
+     * @param resp
+     *            the {@link HttpServletResponse}
+     * @see System#gc()
+     */
+    private void performGarbageCollection() {
+        LOG.info("perfoming gc");
+        try {
+            System.gc();
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "error while performing gc: " + ex.getMessage(), ex);
+        }
+    }
 }
